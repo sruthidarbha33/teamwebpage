9f7b8fb3e319e8a5e4f02228b46509d9f31aa5a9
Bhakti Patel <62191831+bhaktipatel31@users.noreply.github.com>
2021-11-09 18:47:58 -0500

    Update teamwebpage.js

--- teamwebpage.js
+++ teamwebpage.js
@@ -2,17 +2,13 @@ var oldPicture;
 var oldTarget = 0;
 function restorePicture() {
     if (oldTarget == 1) {
-        document.getElementById("laura").src = "https://media-exp1.licdn.com/dms/image/C4D03AQFQn78PrzwV7A/profile-displayphoto-shrink_400_400/0/1602724956102?e=1641427200&v=beta&t=yGmQPYrBhmyzFesoQ8OCQYMMYLlhGH9YFfzOQqwAoHc";
+        document.getElementById("Sejal1").src = "https://i.pinimg.com/236x/02/6a/cc/026acca08fb7beea6bd4ecd430e312bd.jpg";
     }
     if (oldTarget == 2) {
-        document.getElementById("mia").src = "https://i.pinimg.com/280x280_RS/15/a0/b0/15a0b09c9b2bf5f1e20f2fd168bada70.jpg";
+        document.getElementById("Bhakti1").src = "https://i.pinimg.com/564x/40/e6/29/40e629ddb31aedacdf2c5e94cfc7e23a.jpg";
     }
     if (oldTarget == 3) {
-        document.getElementById("indira").src = "https://media-exp1.licdn.com/dms/image/C4D03AQEyNoDRpqX4fA/profile-displayphoto-shrink_800_800/0/1567534037035?e=1641427200&v=beta&t=CEsCPTqKLUcWeGr6UhaPihByjicVuqsAt84elXFWl7I";
-    }
-    if (oldTarget == 4) {
-        document.getElementById("evan").src = "https://media-exp1.licdn.com/dms/image/C5603AQHQHUa8_o0dZg/profile-displayphoto-shrink_800_800/0/1566932666557?e=1641427200&v=beta&t=0E5ARGpYMw0xmG8OfDrXP6MLE7qsvFsM4Y9lmpa-Wy0";
-    }
+        document.getElementById("Sruthi1").src = "https://www.nicepng.com/png/full/161-1615668_super-mario-sunshine-princess-peach.png";
 }
 function toggleVis(id) {
     var x = document.getElementById(id);
@@ -23,41 +19,32 @@ function toggleVis(id) {
     }
 }
 
-function changeLaura(event) {
-    event.target.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.yuDZUz0vA0SpV5W9it8s_wHaFI%26pid%3DApi&f=1";
+function changeSejal(event) {
+    event.target.src = "2nd pic link";
     restorePicture();
     oldTarget = 1;
-    document.getElementById("lauradescription").innerHTML = "Laura is a <strong> senior </strong> majoring in BAIT.";
+    document.getElementById("sejaldescription").innerHTML = "";
 }
 
-document.getElementById("laura").addEventListener("click", changeLaura);
+document.getElementById("Sejal1").addEventListener("click", changesejal);
 
-function changeMia(event) {
-    event.target.src = "https://media-exp1.licdn.com/dms/image/C4D03AQEHjRLpNieiJQ/profile-displayphoto-shrink_200_200/0/1572629093224?e=1641427200&v=beta&t=j3cj8JkvkXz8nycKfSNS0tfFyjmhE6c8NG8xD7X0Xxk";
+function changeBhakti(event) {
+    event.target.src = "https://onmyojiguide.com/wp-content/uploads/2017/01/Glowing-Grass-Onmyoji-Shikigami-Icon-2-1.png";
     restorePicture();
     oldTarget = 2;
-    document.getElementById("miadescription").innerHTML = "Mia is a <strong> senior </strong> majoring in Finance and BAIT.";
+    document.getElementById("bhaktidescription").innerHTML = "Bhakti <strong>loves<strong> Brown Sugar Bubble tea ^_^";
 }
 
-document.getElementById("mia").addEventListener("click", changeMia);
+document.getElementById("Bhakti1").addEventListener("click", changeBhakti);
 
-function changeIndira(event) {
-    event.target.src = "https://scontent-lga3-2.xx.fbcdn.net/v/t1.6435-9/120244545_743584619834145_3286924671549926986_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=-oqG4XqsOn4AX_vo0mr&_nc_ht=scontent-lga3-2.xx&oh=f9d428c1848d7caf8cca45f8ef05745f&oe=61A6010F";
+function changeSruthi(event) {
+    event.target.src = "";
     restorePicture();
     oldTarget = 3;
-    document.getElementById("indiradescription").innerHTML = "Indira is a <strong> senior </strong> majoring in Finance and BAIT.";
+    document.getElementById("sruthidescription").innerHTML = "";
 }
 
-document.getElementById("indira").addEventListener("click", changeIndira);
-
-function changeEvan(event) {
-    event.target.src = "https://miro.medium.com/max/1400/1*KhuDkpTeacAOIYPKjcN8wg.jpeg";
-    restorePicture();
-    oldTarget = 4;
-    document.getElementById("evandescription").innerHTML = "Evan is a <strong> senior </strong> majorirng in BAIT and Supply Chain.";
-}
-
-document.getElementById("evan").addEventListener("click", changeEvan);
+document.getElementById("Sruthi1").addEventListener("click", changeSruthi);
 
 
 
